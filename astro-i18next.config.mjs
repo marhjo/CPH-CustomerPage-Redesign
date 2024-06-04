2a4ce2abd4c0b977873ca9b585400688f78955c4
Hannibal Grønne <hannibalgronne@gmail.com>
2024-06-04 15:04:45 +0200

    Create i18next config with the two languages

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+  defaultLocale: "dk",
+  locales: ["dk", "en"],
+  // routing: {
+  //   prefixDefaultLocale: false,
+  // },
+};

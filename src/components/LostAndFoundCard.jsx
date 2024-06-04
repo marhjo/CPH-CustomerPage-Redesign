28611147262cbb4a5cb6231d33bb657f5ee8a93d
Mark <markhjort99@gmail.com>
2024-06-04 14:59:53 +0200

    Lost and Found Layout Fix

--- src/components/LostAndFoundCard.jsx
+++ src/components/LostAndFoundCard.jsx
@@ -1,5 +1,14 @@
 import clsx from "clsx";
 
 export default function LostAndFoundCard({ children, className }) {
-  return <div className={clsx("flex flex-col shadow-cardMedium p-4 gap-3 justify-center items-center rounded-xl", className)}>{children}</div>;
+  return (
+    <div
+      className={clsx(
+        "flex flex-col shadow-cardMedium p-4 gap-3 justify-start items-center rounded-xl",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
 }

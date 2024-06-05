950fd2de2831c816e5213732f4fffb1a37d93f61
Hannibal Grønne <hannibalgronne@gmail.com>
2024-06-05 10:34:40 +0200

    Changed "dk" to "da"

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 export default {
-  defaultLocale: "dk",
-  locales: ["dk", "en"],
+  defaultLocale: "da",
+  locales: ["da", "en"],
   // routing: {
   //   prefixDefaultLocale: false,
   // },

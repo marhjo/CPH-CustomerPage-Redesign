17573e1c1f8a711dd545004d6c20afc189dca176
Mark <markhjort99@gmail.com>
2024-06-05 10:11:12 +0200

    Stroke Fix

--- src/components/LostAndFound.jsx
+++ src/components/LostAndFound.jsx
@@ -62,9 +62,9 @@ export default function LostAndFound() {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   className="w-4 h-4 mr-2"
                 >
                   <path d="m21 16-4 4-4-4"></path>
@@ -94,9 +94,9 @@ export default function LostAndFound() {
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   className="w-4 h-4 mr-2"
                 >
                   <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon>
